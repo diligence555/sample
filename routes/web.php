579457d5d65070219e651e36c5ef4105dd5ce532
Your Name <2314779927@qq.com>
2018-03-31 12:14:37 +0000

    Finish user edit, update, index, and destroy actions

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,9 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 
+
 //显示登录页面，创建新会话（登录），销毁会话（退出登录）
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
